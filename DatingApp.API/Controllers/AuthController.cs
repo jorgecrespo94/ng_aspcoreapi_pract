159bc926e3b233e758c86877aee30fe752e4df97
jorgecrespo94 <jorgecresporivera@gmail.com>
2019-03-10 18:42:01 -0400

    adding login & attribute auth method protection

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,46 +1,110 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController] // automatically validate and inferre model information through the request
-    public class AuthController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController] // automatically validate and inferre model information through the request
+  public class AuthController : ControllerBase
+  {
+    private readonly IAuthRepository _repo;
+    private readonly IConfiguration _config;
+    public AuthController(IAuthRepository repo, IConfiguration config)
     {
-        private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
-        {
-            _repo = repo;
-        }
-
-        [HttpPost("register")]
-        public async Task<IActionResult> Register(UserForRegisterDto userDTO)
-        {
-            /*
-                if(!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-                Or 
-
-                [ApiController]
-             */
-            
-            userDTO.Username = userDTO.Username.ToLower();
-
-            if(await _repo.UserExists(userDTO.Username))
-                return BadRequest("Username already exists");
-
-            var userToCreate = new User
-            {
-                Username =  userDTO.Username
-            };
-
-            var createdUser = await _repo.Register(userToCreate, userDTO.Username);
-
-            return StatusCode(201);
-        }
+      _repo = repo;
+      _config = config;
     }
-}
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(UserForRegisterDto userDTO)
+    {
+      /*
+            if(!ModelState.IsValid)
+            return BadRequest(ModelState);
+          Or 
+            [ApiController]
+       */
+
+      userDTO.Username = userDTO.Username.ToLower();
+
+      if (await _repo.UserExists(userDTO.Username))
+        return BadRequest("Username already exists");
+
+      var userToCreate = new User
+      {
+        Username = userDTO.Username
+      };
+
+      var createdUser = await _repo.Register(userToCreate, userDTO.Username);
+
+      return StatusCode(201);
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+    {
+      User userFromRepo = await GetDatabaseUser(userForLoginDto);
+
+      if (ObjNotFound(userFromRepo))
+        return Unauthorized();
+
+      var loginClaims = GetLoginClaims(userFromRepo);
+
+      var creds = GetSigningCredentials();
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+
+      var token = GetLoginToken(tokenHandler, loginClaims, creds);
+
+      return Ok(new { token = tokenHandler.WriteToken(token) });
+    }
+
+    private async Task<User> GetDatabaseUser(UserForLoginDto userForLoginDto)
+    {
+      return await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+    }
+
+    private bool ObjNotFound(object obj)
+    {
+      return obj == null;
+    }
+
+    private Claim[] GetLoginClaims(User userFromRepo)
+    {
+      return new[]
+      {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username),
+      };
+    }
+
+    private SigningCredentials GetSigningCredentials()
+    {
+       var key = new SymmetricSecurityKey(Encoding.UTF8
+        .GetBytes(_config.GetSection("AppSettings:Token").Value));//encode key and add to appsetings
+       
+       return new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+    }
+
+    private SecurityToken GetLoginToken(JwtSecurityTokenHandler tokenHandler, Claim[] loginClaims, SigningCredentials creds)
+    {
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims: loginClaims),
+            Expires = DateTime.Now.AddDays(1),
+            SigningCredentials = creds
+        };
+
+        return tokenHandler.CreateToken(tokenDescriptor);
+    }
+  }//end class
+}//end namespace
--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -10,78 +10,70 @@ namespace DatingApp.API.Data
     private readonly DataContext _context;
     public AuthRepository(DataContext context)
     {
-        _context = context;
+      _context = context;
     }
 
     public async Task<User> Login(string username, string password)
     {
-       //get user from db
-       var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+      //get user from db
+      var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
 
-       //stop if user not found
-       if(user == null)
+      //stop if user not found
+      if (user == null)
         return null;
 
       //verify if given password matches
-      if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+      if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
         return null;
 
-      return user;  
+      return user;
     }
 
     //created a new hash with the users stored key
     //then compare both have to see if they are equal.
     private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
     {
-       byte[] computedHash = null;
-
-       using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+      using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+      {
+        var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        for (int i = 0; i < computedHash.Length; i++)
         {
-          computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+          if (computedHash[i] != passwordHash[i]) { return false; }
         }
-
-        return (IsPasswordHashEquals(passwordHash, computedHash));
-    }
-
-    private bool IsPasswordHashEquals(byte[] passwordHash, byte[] computedHash)
-    {
-        for(int i = 0; i < computedHash.Length; i++)          
-          if(computedHash[i] != passwordHash[i]) return false;
-          
-        return true;//all byte are equal
+        return true;
+      }
     }
 
     public async Task<User> Register(User user, string password)
     {
-        //convert password to password hash/salt
-        byte[] passswordHash, passwordSalt;
-        CreatePasswordHash(password, out passswordHash, out passwordSalt);
-        
-        //get values
-        user.PasswordHash = passswordHash;
-        user.PasswordSalt = passwordSalt;
+      //convert password to password hash/salt
+      byte[] passswordHash, passwordSalt;
+      CreatePasswordHash(password, out passswordHash, out passwordSalt);
 
-        //save values
-        await _context.Users.AddAsync(user);
-        await _context.SaveChangesAsync();
-        
-        return user;
+      //get values
+      user.PasswordHash = passswordHash;
+      user.PasswordSalt = passwordSalt;
+
+      //save values
+      await _context.Users.AddAsync(user);
+      await _context.SaveChangesAsync();
+
+      return user;
     }
 
     //create password hash with random salt
-    private void CreatePasswordHash(string password, out byte[] passswordHash, out byte[] passwordSalt)
+    private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
     {
-        using (var hmac = new System.Security.Cryptography.HMACSHA512())
-        {
-            passwordSalt = hmac.Key;
-            passswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-        }
+      using (var hmac = new System.Security.Cryptography.HMACSHA512())
+      {
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+      }
     }
-
     //return whether or not the username is present on the database
     public async Task<bool> UserExists(string username)
     {
-      return (await _context.Users.AnyAsync( x=> x.Username == username));
+      return (await _context.Users.AnyAsync(x => x.Username == username));
     }
 
   }
