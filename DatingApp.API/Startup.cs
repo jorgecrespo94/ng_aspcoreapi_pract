d65cd00908882fc1b2fef276f5b7e01ac7105ca1
jorgecrespo94 <jorgecresporivera@gmail.com>
2019-04-11 23:40:14 -0400

    adding upload photo to API

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -42,6 +42,7 @@ namespace DatingApp.API
                     opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
             services.AddTransient<Seed>();
             //services.AddSingleton() //- we create a single instance through out the app
