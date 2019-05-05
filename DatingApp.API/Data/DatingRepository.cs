27947f0d92b2709f8507143907f01b1a0fe99d2a
jorgecrespo94 <jorgecresporivera@gmail.com>
2019-05-05 17:08:54 -0400

    added pagination

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Helpers;
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
@@ -42,10 +44,10 @@ namespace DatingApp.API.Data
         return user;
     }
 
-    public async Task<IEnumerable<User>> GetUsers()
+    public async Task<PagedList<User>> GetUsers([FromQuery]UserParams userParams)
     {
-       var users = await _context.Users.Include(p => p.Photos).ToListAsync();
-       return users;
+       var users = _context.Users.Include(p => p.Photos);
+       return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
     }
 
     public async Task<bool> SaveAll()
